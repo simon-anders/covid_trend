dd43927276d780f2956e137b8aa558bc2c6d79aa
Simon Anders <sanders@fs.tum.de>
2020-03-24 00:11:17 +0100

    rm unused css, console log

--- toggle_traces.js
+++ toggle_traces.js
@@ -1,12 +1,10 @@
 toggle = function(){
-   console.log("AB")
    legendtoggles = document.getElementsByClassName("legendtoggle")
    legendtexts = document.getElementsByClassName("legendtext")
    toggled = false
    for( i=0; i < legendtoggles.length; i++ ) {
    	   country = legendtexts[i].getAttribute("data-unformatted")
    	   if( country == "Germany" ) {
-   	   	   console.log( country )
 		   legendtoggles[i].dispatchEvent( new Event("mousedown") )
 		   legendtoggles[i].dispatchEvent( new Event("mouseup") )
 		   legendtoggles[i].dispatchEvent( new Event("mousedown") )
